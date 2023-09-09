db51af40c9c40d8ccfd72a4f12a0702e7eedbff5
doganciftcir6 <114991786+doganciftcir6@users.noreply.github.com>
2023-09-09 20:34:48 +0300

    feat: InitialCreate

---
+++ DockerCompose/MicroService1.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using MicroService1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+            _context.Products.Add(new Product() { Name = "Pen 1" });
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"{_configuration.GetSection("Microservices")["baseUrlMicroservice2"]}/api/prodcuts");
+            var context = await response.Content.ReadAsStringAsync();
+            return Ok(context);
+        }
+    }
+}
---
+++ DockerCompose/Microservice2.API/Controllers/ProdcutsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdcutsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { id = 1, name = "Pencil 2 (Microservice 2)" });
+        }
+    }
+}
